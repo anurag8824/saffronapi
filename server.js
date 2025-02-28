f904329ea8e7c5129b261c19a8398f72a7c567dc
anurag8824 <infayoudigital@gmail.com>
2025-02-28 14:47:57 +0530

    g

--- server.js
+++ server.js
@@ -98,16 +98,158 @@ app.get("/detailresult/:id/:mid", async (req, res) => {
     }
 });
 
-
-
-
-
-app.get("/getGameList", async (req, res) => {
+//sports api endpoints 
+// 
+// 
+// //get all sportslist 
+app.get("/allSportsList", async (req, res) => {
     try {
-        const response = await axios.get("https://dream.bagpackkar.com/d110923/shyamp/getGames", {
-            headers: {
-                // 'x-igtechdiamondsports-key': '67976dfa6c0d3c07d8b3b1ff',
+        const response = await axios.get("http://45.198.14.150:8085/sports/allSportid", {
+                headers: {
+                'x-igtechdiamondsports-key': '67976dfa6c0d3c07d8b3b1ff',
+                }
+        });
+        res.json(response.data);
+        console.log("Fetched Data:", response);
+    } catch (error) {
+        console.error("Error fetching table list:", error.response?.data || error.message);
+        res.status(error.response?.status || 500).json({ error: error.response?.data || "Failed to fetch table list" });
+    }
+});
 
+//get game detail 
+app.get("/gameDetails/:sid/:gmid", async (req, res) => {
+    const sportId = req.params.sid
+    const gameId = req.params.gmid
+
+
+    try {
+        const response = await axios.get(`http://45.198.14.150:8085/sports/getDetailsData?sid=${sportId}&gmid=${gameId}`, {
+            headers: {
+                'x-igtechdiamondsports-key': '67976dfa6c0d3c07d8b3b1ff',
+                }
+        });
+        res.json(response.data);
+        console.log("Fetched Data:", response);
+    } catch (error) {
+        console.error("Error fetching table list:", error.response?.data || error.message);
+        res.status(error.response?.status || 500).json({ error: error.response?.data || "Failed to fetch table list" });
+    }
+});
+
+//get all match data
+app.get("/allMatchData/:sid/:gmid", async (req, res) => {
+    const sportId = req.params.sid
+    const gameId = req.params.gmid
+
+
+    try {
+        const response = await axios.get(`http://45.198.14.150:8085/sports/getPriveteData?sid=${sportId}&gmid=${gameId}`, {
+            headers: {
+                'x-igtechdiamondsports-key': '67976dfa6c0d3c07d8b3b1ff',
+                }
+        });
+        res.json(response.data);
+        console.log("Fetched Data:", response);
+    } catch (error) {
+        console.error("Error fetching table list:", error.response?.data || error.message);
+        res.status(error.response?.status || 500).json({ error: error.response?.data || "Failed to fetch table list" });
+    }
+});
+
+// get all match using sport id
+app.get("/allMatchUsingSports/:sid", async (req, res) => {
+    const sportId = req.params.sid
+
+
+    try {
+        const response = await axios.get(`http://45.198.14.150:8085/sports/esid?sid=${sportId}`, {
+            headers: {
+                'x-igtechdiamondsports-key': '67976dfa6c0d3c07d8b3b1ff',
+                }
+        });
+        res.json(response.data);
+        console.log("Fetched Data:", response);
+    } catch (error) {
+        console.error("Error fetching table list:", error.response?.data || error.message);
+        res.status(error.response?.status || 500).json({ error: error.response?.data || "Failed to fetch table list" });
+    }
+});
+
+//get live tv
+
+app.get("/liveTv/:gmid", async (req, res) => {
+    const gameId = req.params.gmid
+
+    try {
+        const response = await axios.get(`http://45.198.14.150:8085/sports/tv?gmid=${gameId}`, {
+            headers: {
+                'x-igtechdiamondsports-key': '67976dfa6c0d3c07d8b3b1ff',
+                }
+        });
+        res.json(response.data);
+        console.log("Fetched Data:", response);
+    } catch (error) {
+        console.error("Error fetching table list:", error.response?.data || error.message);
+        res.status(error.response?.status || 500).json({ error: error.response?.data || "Failed to fetch table list" });
+    }
+});
+
+
+//get all game details 
+app.get("/allGameDetails", async (req, res) => {
+
+
+
+    try {
+        const response = await axios.get(`http://45.198.14.150:8085/sports/tree`, {
+            headers: {
+                'x-igtechdiamondsports-key': '67976dfa6c0d3c07d8b3b1ff',
+                }
+        });
+        res.json(response.data);
+        console.log("Fetched Data:", response);
+    } catch (error) {
+        console.error("Error fetching table list:", error.response?.data || error.message);
+        res.status(error.response?.status || 500).json({ error: error.response?.data || "Failed to fetch table list" });
+    }
+});
+
+
+// get sports score 
+app.get("/sportsScore/:sid/:gtv", async (req, res) => {
+
+    const sportId = req.params.sid
+    const gtv = req.params.gtv
+
+    try {
+        const response = await axios.get(`http://45.198.14.150:8085/sports/score?sportid=${sportId}&gtv=${gtv}`, {
+            headers: {
+                'x-igtechdiamondsports-key': '67976dfa6c0d3c07d8b3b1ff',
+                }
+        });
+        res.json(response.data);
+        console.log("Fetched Data:", response);
+    } catch (error) {
+        console.error("Error fetching table list:", error.response?.data || error.message);
+        res.status(error.response?.status || 500).json({ error: error.response?.data || "Failed to fetch table list" });
+    }
+});
+
+
+// get  one result
+
+app.post("/resultone", async (req, res) => {
+    const { team_one, team_two, sport_id } = req.body
+    try {
+        const response = await axios.post(`http://45.198.14.150:8085/sports/result`, {
+            headers: {
+                'x-igtechdiamondsports-key': '67976dfa6c0d3c07d8b3b1ff',
+                },
+            body: {
+                team_one: team_one,
+                team_two: team_two,
+                sport_id: sport_id,
             }
         });
         res.json(response.data);
@@ -119,119 +261,33 @@ app.get("/getGameList", async (req, res) => {
 });
 
 
-app.get("/allseries/:id", async (req, res) => {
-    const sportId = req.params.id
-    try {
-        const response = await axios.get(`https://dream.bagpackkar.com/d110923/shyamp/getSeries?sport_id=${sportId}`, {
-            headers: {
-//                'x-igtechdiamondsports-key': '67976dfa6c0d3c07d8b3b1ff',
+// get result two 
 
+
+app.post("/resulttwo", async (req, res) => {
+    const { team_one, team_two, sport_id } = req.body
+    try {
+        const response = await axios.post(`http://45.198.14.150:8085/sports/result-correct`, {
+            headers: {
+                'x-igtechdiamondsports-key': '67976dfa6c0d3c07d8b3b1ff',
+                },
+            body: {
+                team_one: team_one,
+                team_two: team_two,
+                sport_id: sport_id,
             }
         });
         res.json(response.data);
         console.log("Fetched Data:", response);
     } catch (error) {
-        console.error("Error fetching frame url:", error.response?.data || error.message);
-        res.status(error.response?.status || 500).json({ error: error.response?.data || "Failed to fetch frame url" });
+        console.error("Error fetching table list:", error.response?.data || error.message);
+        res.status(error.response?.status || 500).json({ error: error.response?.data || "Failed to fetch table list" });
     }
 });
 
 
 
-app.get("/getmatches/:id/:sid", async (req, res) => {
-    const sportId = req.params.id
-    const seriesId = req.params.sid
 
-    try {
-        const response = await axios.get(`https://dream.bagpackkar.com/d110923/shyamp/getMatches?series_id=${seriesId}&game_id=${sportId}
-`, {
-            headers: {
-                // 'x-igtechdiamondsports-key': '67976dfa6c0d3c07d8b3b1ff',
-
-            }
-        });
-        res.json(response.data);
-        console.log("Fetched Data:", response);
-    } catch (error) {
-        console.error("Error fetching casino data:", error.response?.data || error.message);
-        res.status(error.response?.status || 500).json({ error: error.response?.data || "Failed to fetch casino data" });
-    }
-});
-
-
-app.get("/matchinfo/:id", async (req, res) => {
-    const matchId = req.params.id
-    try {
-        const response = await axios.get(`https://dream.bagpackkar.com/d110923/shyamp/getMarket?match_id=${matchId}`, {
-            headers: {
-                // 'x-igtechdiamondsports-key': '67976dfa6c0d3c07d8b3b1ff',
-
-            }
-        });
-        res.json(response.data);
-        console.log("Fetched Data:", response);
-    } catch (error) {
-        console.error("Error fetching casino result:", error.response?.data || error.message);
-        res.status(error.response?.status || 500).json({ error: error.response?.data || "Failed to fetch casino result" });
-    }
-});
-
-
-
-app.get("/getfancy/:id", async (req, res) => {
-    const matchId = req.params.id
-    try {
-        const response = await axios.get(`https://dream.bagpackkar.com/api/switch/getFancy?matchId=${matchId}`, {
-            headers: {
-                // 'x-igtechdiamondsports-key': '67976dfa6c0d3c07d8b3b1ff',
-
-            }
-        });
-        res.json(response.data);
-        console.log("Fetched Data:", response);
-    } catch (error) {
-        console.error("Error fetching casino detail result:", error.response?.data || error.message);
-        res.status(error.response?.status || 500).json({ error: error.response?.data || "Failed to fetch casino detail result" });
-    }
-});
-
-
-app.get("/getbm/:id", async (req, res) => {
-    const matchId = req.params.id
-    try {
-        const response = await axios.get(`https://dream.bagpackkar.com/api/switch/getBM?matchId=${matchId}`, {
-            headers: {
-                // 'x-igtechdiamondsports-key': '67976dfa6c0d3c07d8b3b1ff',
-
-            }
-        });
-        res.json(response.data);
-        console.log("Fetched Data:", response);
-    } catch (error) {
-        console.error("Error fetching casino detail result:", error.response?.data || error.message);
-        res.status(error.response?.status || 500).json({ error: error.response?.data || "Failed to fetch casino detail result" });
-    }
-});
-
-
-
-app.get("/getoddslist", async (req, res) => {
-    // const matchId = req.params.id
-    try {
-        const response = await axios.get(`https://dream.bagpackkar.com/d110923/shyamp/getOddsList?marketid=1.224125101,1.224125101,1.224125101,1.224125101,1.224125101,1.224125101,1.224125101
-`, {
-            headers: {
-                // 'x-igtechdiamondsports-key': '67976dfa6c0d3c07d8b3b1ff',
-
-            }
-        });
-        res.json(response.data);
-        console.log("Fetched Data:", response);
-    } catch (error) {
-        console.error("Error fetching casino detail result:", error.response?.data || error.message);
-        res.status(error.response?.status || 500).json({ error: error.response?.data || "Failed to fetch casino detail result" });
-    }
-});
 
 
 // Set up the server to listen on port 3000
